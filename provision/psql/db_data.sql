921e5d1cafbbb88de358b18fb5a1836f8f834f90
lydiadwyer <lydialdwyer@gmail.com>
2016-09-05 15:00:28 -0400

    cities unit testing updates

--- provision/psql/db_data.sql
+++ provision/psql/db_data.sql
@@ -3,9 +3,9 @@ INSERT INTO artifacts(artifact_name, artifact_obj_reg) VALUES('test2', 112);
 INSERT INTO artifacts(artifact_name, artifact_obj_reg) VALUES('test3', 113);
 
 
-INSERT INTO excavations(excavation_name) VALUES('test1');
-INSERT INTO excavations(excavation_name) VALUES('test2');
-INSERT INTO excavations(excavation_name) VALUES('test3');
+INSERT INTO excavations(excavation_name) VALUES('Idalion', 1, 1, 1);
+INSERT INTO excavations(excavation_name) VALUES('Wine Cave', 2, 2, 2);
+INSERT INTO excavations(excavation_name) VALUES('Catal Houyouk', 3, 3, 3);
 
 INSERT INTO countries(country_name) VALUES('Cyprus');
 INSERT INTO countries(country_name) VALUES('France');
@@ -15,6 +15,6 @@ INSERT INTO regions(region_name, country_id) VALUES('Dali', 1);
 INSERT INTO regions(region_name, country_id) VALUES('Loire Valley', 2);
 INSERT INTO regions(region_name, country_id) VALUES('Ankara', 3);
 
-INSERT INTO cities(city_name, country_id, region_id) VALUES('testy', 1, 1);
-INSERT INTO cities(city_name, country_id, region_id) VALUES('testy2', 2, 2);
-INSERT INTO cities(city_name, country_id, region_id) VALUES('testy3', 3, 3);
+INSERT INTO cities(city_name, country_id, region_id) VALUES('Dali', 1, 1);
+INSERT INTO cities(city_name, country_id, region_id) VALUES('Tours', 2, 2);
+INSERT INTO cities(city_name, country_id, region_id) VALUES('Istanbul', 3, 3);
