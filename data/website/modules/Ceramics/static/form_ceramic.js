a4e6acdd7e5d8b847496119b318e18b5fb6b8337
lydiadwyer <lydialdwyer@gmail.com>
2016-10-11 17:26:35 -0400

    Edited to make some validation rules. Rules not complete.

--- data/website/modules/Ceramics/static/form_ceramic.js
+++ data/website/modules/Ceramics/static/form_ceramic.js
@@ -6,10 +6,13 @@ function isAlpha(xStr){
     var regEx = /^[a-zA-Z ]+$/;
     return xStr.match(regEx);
 }
-function isUpper(xStr){
-    var regEx = /^[A-Z ]+$/;
+
+function isNum(xStr){
+    var regEx = /^[0-9 ]+$/;
     return xStr.match(regEx);
 }
+
+
 function verify_ceramic() {
 
     // flag for any value errors
@@ -35,51 +38,64 @@ function verify_ceramic() {
     var excavated_from_msg = document.getElementById("excavated_from_msg");
 
 
-    // clear out the error message
-    country_name_msg.innerHTML = "";
-    // clear out the error message
-    country_abrev_msg.innerHTML = "";
+    // clear out the error messages
+    ceramic_type_msg.innerHTML = "";
+    ceramic_ware_msg.innerHTML = "";
+    ceramic_form_msg.innerHTML = "";
+    ceramic_reg_id_msg.innerHTML = "";
+    date_period_msg.innerHTML = "";
+    excavated_from_msg.innerHTML = "";
 
-    // ensure country_name is not null
-    if(!country_name || "" == country_name) {
-        country_name_msg.innerHTML = "Please fill in the country name.";
+    // Check all to make sure they are filled in
+    // ensure ceramic_type is not null
+    if(!ceramic_type || "" == ceramic_type) {
+        ceramic_type_msg.innerHTML = "Please fill in the ceramic type.";
+        form_is_valid = false;
+    }
+    // ensure ceramic_ware is not null
+    if(!ceramic_ware || "" == ceramic_ware) {
+        ceramic_ware_msg.innerHTML = "Please fill in the ceramic ware.";
+        form_is_valid = false;
+    }
+    // ensure ceramic_form is not null
+    if(!ceramic_form || "" == ceramic_form) {
+        ceramic_form_msg.innerHTML = "Please fill in the ceramic form.";
+        form_is_valid = false;
+    }
+    // ensure ceramic_reg_id is not null
+    if(!ceramic_reg_id || "" == ceramic_reg_id) {
+        ceramic_reg_id_msg.innerHTML = "Please fill in the ceramic registration id.";
+        form_is_valid = false;
+    }
+    // ensure date_period is not null
+    if(!date_period || "" == date_period) {
+        date_period_msg.innerHTML = "Please fill in the era.";
+        form_is_valid = false;
+    }
+    // ensure excavated_from is not null
+    if(!excavated_from || "" == excavated_from) {
+        excavated_from_msg.innerHTML = "Please fill in the excavation site.";
         form_is_valid = false;
     }
 
-    // ensure country_name is longer than 3 characters
-    if(!country_name_msg.innerHTML && country_name.length < 4) {
-        country_name_msg.innerHTML = "Please fill in the country name completely.";
+    // Check all to make sure they are not too long (less than 128 chars), and >0
+    // Check that the right data are being input, strings or numbers
+
+    // ensure ceramic_type is longer than 1 character
+    if(!ceramic_type.innerHTML && ceramic_type.length < 2) {
+        ceramic_type_msg.innerHTML = "Please fill in the ceramic type completely.";
+        form_is_valid = false;
+    }
+    // ensure ceramic_type is less than 128 characters
+    if(!ceramic_type_msg.innerHTML && ceramic_type.length > 128) {
+        ceramic_type_msg.innerHTML = "Please fill in a shorter ceramic type name.";
         form_is_valid = false;
     }
 
-    // ensure country_name is less than 33 characters
-    if(!country_name_msg.innerHTML && country_name.length > 32) {
-        country_name_msg.innerHTML = "Please fill in a shorter country name.";
-        form_is_valid = false;
-    }
 
     // character check for country_name
-    if(!country_name_msg.innerHTML && !isAlpha(country_name)) {
-        country_name_msg.innerHTML = "Please fill in a country name only with English letters.";
-        form_is_valid = false;
-    }
-
-    // Country abbreviation validation
-    // ensure country_abrev is not null
-    if(!country_abrev || "" == country_abrev) {
-        country_abrev_msg.innerHTML = "Please fill in the country abbreviation.";
-        form_is_valid = false;
-    }
-
-    // ensure country_abrev is 2 characters
-    if(!country_abrev_msg.innerHTML && 2 !== country_abrev.length) {
-        country_abrev_msg.innerHTML = "Please fill in the country abbreviation with 2 characters.";
-        form_is_valid = false;
-    }
-
-    // character check for country_abrev
-    if(!country_abrev_msg.innerHTML && !isUpper(country_abrev)) {
-        country_abrev_msg.innerHTML = "Please fill in the country abbreviation using only uppercase letters.";
+    if(!ceramic_type_msg.innerHTML && !isAlpha(ceramic_type)) {
+        ceramic_type_msg.innerHTML = "Please fill in a ceramic type only with English letters.";
         form_is_valid = false;
     }
 
